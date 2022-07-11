15304bdb378a476897cd3cd6e7d8f871ee11665f
ericlinqq <fictionman66@icloud.com>
2022-07-11 16:36:56 +0800

    NN class done and everything works fine, result of linear data.

--- lab1/Backpropagation.py
+++ lab1/Backpropagation.py
@@ -1,9 +1,16 @@
 #%%
-from re import L
-from selectors import EpollSelector
+%matplotlib inline
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+def derivative_sigmoid(x):
+    """
+    Input: the value that output from sigmoid function.
+    """
+    return np.multiply(x, 1.0 - x)
 
 class DataGenerator :
     def __init__(self, type):
@@ -13,7 +20,8 @@ class DataGenerator :
         assert(type == 'Linear' or type == 'XOR')
         self.__type = type
     
-    def __generate_linear(n):
+    @staticmethod
+    def generate_linear(n):
         pts = np.random.uniform(0, 1, (n, 2))
         inputs = []
         labels = []
@@ -26,7 +34,8 @@ class DataGenerator :
                 labels.append(1)
         return np.array(inputs), np.array(labels).reshape(n, 1)
 
-    def __generate_XOR_easy():
+    @staticmethod
+    def generate_XOR_easy():
         inputs = []
         labels = []
         
@@ -43,204 +52,256 @@ class DataGenerator :
 
     def get_data(self, n=100):
         func_dict = {
-            'Linear': DataGenerator.__generate_linear(n),
-            'XOR': DataGenerator.__generate_XOR_easy()
+            'Linear': DataGenerator.generate_linear(n),
+            'XOR': DataGenerator.generate_XOR_easy()
         }
         func = func_dict[self.__type]
 
         return func
-        
-def init_parameter(layer_dims):
-    np.random.seed(56)
-    parameters = {}
-    Layers = len(layer_dims)
+
+class NN:
+    def __init__(self, layer_dims, epoch=3000, lr=0.08, print_step=100):
+        """
+        Args: 
+            layer_dims: a list of the dims in each layer (include input and output layer) [2, dim1, dim2, ..., 1].
+        """
+        self.set_layer_dims(layer_dims)
+        self.__epoch = epoch
+        self.__lr = lr
+        self.__print_step = print_step
     
-    # Initialize weight matrix
-    for l in range(1, Layers):
-        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1])
-        parameters['b' + str(l)] = np.zeros([layer_dims[l], 1])
-
-    return parameters
-
-def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-x))
-
-def derivative_sigmoid(x):
-    return np.multiply(x, 1.0 - x)
-
-def forward_pass(X, parameters):
-    w_num = len(parameters) // 2
-    A = X.T
-    records = []
-
-    for i in range(1, w_num+1):
-        linear_record = {}
-        activation_record = {}
-        A_prev = A
-        W = parameters['W' + str(i)]
-        b = parameters['b' + str(i)]
-
-        # Linear calculation
-        linear_record['W' + str(i)] = W
-        linear_record['b' + str(i)] = b
-        linear_record['A_prev' + str(i)] = A_prev
-        Z = np.dot(W, A_prev) + b
-
-        # sigmoid calculation
-        A = sigmoid(Z)
-        activation_record['Z' + str(i)] = Z
-        # print(A.shape)
-
-        record = (linear_record, activation_record)
-        records.append(record)
-
-    assert(A.shape == (1, X.shape[0]))
-
-    return A, records
-
-
-def loss(predict, label):
-    m = label.shape[0]
+    def set_layer_dims(self, layer_dims):
+        assert isinstance(layer_dims, list)
+        self.__layer_dims = layer_dims
     
-    ce = (1./m) * (-np.dot(label.T, np.log(predict).T) - np.dot((1-label).T, np.log(1-predict).T))
-    return ce
-
-def backward_pass(AL, X, Y, records):
-    grads = {}
-    L = len(records)
-    m = AL.shape[1]
+    def init_parameters(self):
+        """
+        Returns:
+            A dictionary of parameters that contains the following:
+                W(l): Weight matrix in each layer (l-th_dim, (l-1)-th_dim) ndarray
+                b(l);: Bias vector in each layer (l-th_dim, 1) ndarray
+        """
+        np.random.seed(56)
+        parameters = {}
+        Layers = len(self.__layer_dims)
         
-    dC_dAL = - (np.divide(Y.T, AL) - np.divide(1-Y.T, 1-AL))
-    dC_dZL = dC_dAL * derivative_sigmoid(AL)
-    assert(dC_dZL.shape == AL.shape)
-
-    linear_record, activation_record = records[-1]
-    A_prev = linear_record['A_prev' + str(L)]
-    W = linear_record['W' + str(L)]
-    b = linear_record['b' + str(L)]
-    m = A_prev.shape[1]
-    dC_dW = 1./m * np.dot(dC_dZL, A_prev.T)
-    dC_db = 1./m * np.sum(dC_dZL, axis=1, keepdims=True)
-
-    dC_dAprev = np.dot(W.T, dC_dZL)
-    assert (dC_dAprev.shape == A_prev.shape)  # check shape
-    assert (dC_dW.shape == W.shape)
-    assert (dC_db.shape == b.shape)
+        # Initialize weight and bias
+        for l in range(1, Layers):
+            parameters['W' + str(l)] = np.random.randn(self.__layer_dims[l], self.__layer_dims[l-1])
+            parameters['b' + str(l)] = np.zeros([self.__layer_dims[l], 1])
+        
+        return parameters
     
-    grads['dA' + str(L-1)], grads['dW' + str(L)], grads['db' + str(L)] = dC_dAprev, dC_dW, dC_db
-    
-    for l in reversed(range(1, L)):
-        linear_record, activation_record= records[l-1]
+    def forward_pass(self, X, parameters):
+        """
+        Args:
+            X: Input data (data_number, 2) ndarray. 
+        Returns:
+            A: The result of passing input through the whole network. (1, data_num) ndarray
+            records: A list of tuple (linear_record, activation_record) in each layer.
+                linear_record: Weight, bias and the input before linear calculation.
+                activation_record: The input before activation calculation.
+        """
+        w_num = len(parameters) // 2
+        A = X.T
+        records = []
+
+        for i in range(1, w_num+1):
+            linear_record = {}
+            activation_record = {}
+            A_prev = A
+            W = parameters['W' + str(i)]
+            b = parameters['b' + str(i)]
+
+            # Linear calculation
+            linear_record['W' + str(i)] = W
+            linear_record['b' + str(i)] = b
+            linear_record['A_prev' + str(i)] = A_prev
+            Z = np.dot(W, A_prev) + b
+
+            # Activation calculation
+            activation_record['Z' + str(i)] = Z 
+            A = sigmoid(Z)
+
+            record = (linear_record, activation_record)
+            records.append(record)
+
+        assert(A.shape == (1, X.shape[0]))
+
+        return A, records
+
+    @staticmethod
+    def compute_loss(predict, label):
+        """
+        Cross entropy
+        """
+        m = label.shape[0]
         
-        Z = activation_record['Z' + str(l)]
-        dC_dZ = np.array(grads["dA" + str(l)]) * derivative_sigmoid(sigmoid(Z))
-        assert (dC_dZ.shape == Z.shape)  
-        
-        A_prev= linear_record['A_prev' + str(l)]
-        W = linear_record['W'+ str(l)]
-        b = linear_record['b' + str(l)]
+        ce = (1./m) * (-np.dot(label.T, np.log(predict).T) - np.dot((1-label).T, np.log(1-predict).T))
+        return ce
+
+    def backward_pass(self, AL, X, Y, records):
+        """
+        Args:
+            AL: The result of passing input through the whole network. (1, data_num) ndarray
+            X: Input data (data_num, 2) ndarray
+            Y: Ground-Truth of the input data. (data_num, 1) ndarray
+            records: A list of tuple (linear_record, activation_record) in each layer.
+        Returns:
+            grads: A dictionary of dC_dA(l-1), dC_dW(l), dC_db(l) matrices.
+                dC_dA(l-1): ((l-1)-th_dim, data_num) ndarray (same as A(l-1))
+                dC_dW(l): (l-th_dim, (l-1)th_dim) ndarrray (same as W(l))
+                dC_db(l): (l-th_dim, 1) ndarray (same as b(l))
+        """
+        grads = {}
+        L = len(records)
+        m = AL.shape[1]
+
+        ## The L-th layer    
+        dC_dAL = - (np.divide(Y.T, AL) - np.divide(1-Y.T, 1-AL))
+        dC_dZL = dC_dAL * derivative_sigmoid(AL)
+        assert(dC_dZL.shape == AL.shape)
+
+        linear_record, activation_record = records[-1]
+        A_prev = linear_record['A_prev' + str(L)]
+        W = linear_record['W' + str(L)]
+        b = linear_record['b' + str(L)]
         m = A_prev.shape[1]
-        dC_dW = 1./m * np.dot(dC_dZ, A_prev.T)
-        dC_db = 1./m * np.sum(dC_dZ, axis = 1, keepdims = True)
-        dC_dAprev = np.dot(W.T, dC_dZ)
+        dC_dW = 1./m * np.dot(dC_dZL, A_prev.T)
+        dC_db = 1./m * np.sum(dC_dZL, axis=1, keepdims=True)
 
-        assert (dC_dAprev.shape == A_prev.shape)
+        dC_dAprev = np.dot(W.T, dC_dZL)
+        assert (dC_dAprev.shape == A_prev.shape) 
         assert (dC_dW.shape == W.shape)
         assert (dC_db.shape == b.shape)
-
-        grads["dA" + str(l-1)], grads["dW" + str(l)], grads["db" + str(l)] = dC_dAprev, dC_dW, dC_db
-
-    return grads
-
-def update_parameters(parameters, grads, learning_rate):
-    L = len(parameters) // 2
-
-    for l in range(L):
-        parameters['W' + str(l+1)] -= learning_rate*grads['dW' + str(l+1)]
-        parameters['b' + str(l+1)] -= learning_rate*grads['db' + str(l+1)]
-
-    return parameters
-
-
-def model(X, Y, layer_dims, epoch=3000, learning_rate=0.08, print_step=1000):
-    costs = []
-    parameters = init_parameter(layer_dims)
-
-    for i in range(epoch):
-        AL, records = forward_pass(X, parameters)
-
-        cost = loss(AL, Y)
-
-        grads = backward_pass(AL, X, Y, records)
-
-        parameters = update_parameters(parameters, grads, learning_rate)
-
-        if i % print_step == 0:
-            print(f"Epoch: {i} Loss: {cost}\n")
-
-        costs.append(cost)
-
-    return parameters 
-
-def predict(X, Y, parameters):
-    m = X.shape[0]
-    n = len(parameters) // 2
-    pred = np.zeros([1, m])
-
-    AL, records = forward_pass(X, parameters)
-    
-    for i in range(m):
-        if AL[0, i] > 0.5:
-            pred[0, i] = 1
-        else:
-            pred[0, i] = 0
-
-    correct_num = np.sum(pred == Y.T)
-    wrong_num = m - correct_num
-    accuracy = correct_num / m 
-    print(f"Accuracy: {accuracy}\n")
-    print(f"Wrong Prediction Count: {wrong_num}\n")
-
-    return AL, pred
-
-def show_result(x, y, pred_y):
-    plt.subplot(1, 2, 1)
-    plt.title('Ground truth', fontsize=18)
-    for i in range(x.shape[0]):
-        if y[i] == 0:
-            plt.plot(x[i][0], x[i][1], 'ro')
-        else:
-            plt.plot(x[i][0], x[i][1], 'bo')
-
-    plt.subplot(1, 2, 2)
-    plt.title('Predict result', fontsize=18)
-    for i in range(x.shape[0]):
-        if pred_y[0][i] == 0:
-            plt.plot(x[i][0], x[i][1], 'ro')
-        else:
-            plt.plot(x[i][0], x[i][1], 'bo')
         
-    plt.show() 
+        grads['dA' + str(L-1)], grads['dW' + str(L)], grads['db' + str(L)] = dC_dAprev, dC_dW, dC_db
+
+        # (L-1)th ~ 1st layer
+        for l in reversed(range(1, L)):
+            linear_record, activation_record= records[l-1]
+            
+            Z = activation_record['Z' + str(l)]
+            dC_dZ = np.array(grads["dA" + str(l)]) * derivative_sigmoid(sigmoid(Z))
+            assert (dC_dZ.shape == Z.shape)  
+            
+            A_prev= linear_record['A_prev' + str(l)]
+            W = linear_record['W'+ str(l)]
+            b = linear_record['b' + str(l)]
+            m = A_prev.shape[1]
+            dC_dW = 1./m * np.dot(dC_dZ, A_prev.T)
+            dC_db = 1./m * np.sum(dC_dZ, axis = 1, keepdims = True)
+            dC_dAprev = np.dot(W.T, dC_dZ)
+
+            assert (dC_dAprev.shape == A_prev.shape)
+            assert (dC_dW.shape == W.shape)
+            assert (dC_db.shape == b.shape)
+
+            grads["dA" + str(l-1)], grads["dW" + str(l)], grads["db" + str(l)] = dC_dAprev, dC_dW, dC_db
+
+        return grads
+
+    def update_parameters(self, parameters, grads):
+        L = len(parameters) // 2
+
+        for l in range(L):
+            parameters['W' + str(l+1)] -= self.__lr*grads['dW' + str(l+1)]
+            parameters['b' + str(l+1)] -= self.__lr*grads['db' + str(l+1)]
+
+        return parameters
+
+
+    def train(self, X, Y):
+        costs = []
+        parameters = self.init_parameters()
+
+        for i in range(self.__epoch):
+            AL, records = self.forward_pass(X, parameters)
+
+            loss = NN.compute_loss(AL, Y)
+
+            grads = self.backward_pass(AL, X, Y, records)
+
+            parameters = self.update_parameters(parameters, grads)
+
+            if i % self.__print_step == 0:
+                print(f"Epoch: {i} Loss: {loss}\n")
+
+            costs.append(loss[0][0])
+
+        return parameters, costs 
+
+    def predict(self, X, Y, parameters):
+        m = X.shape[0]
+        n = len(parameters) // 2
+        pred = np.zeros([1, m])
+
+        AL, records = self.forward_pass(X, parameters)
+        
+        for i in range(m):
+            if AL[0, i] >= 0.5:
+                pred[0, i] = 1
+            else:
+                pred[0, i] = 0
+
+        correct_num = np.sum(pred == Y.T)
+        wrong_num = m - correct_num
+        accuracy = correct_num / m 
+        print(f"Accuracy: {accuracy}\n")
+        print(f"Wrong Prediction Count: {wrong_num}\n")
+
+        return AL, pred
+
+    @staticmethod
+    def show_result(x, y, pred_y):
+        plt.subplot(1, 2, 1)
+        plt.title('Ground truth', fontsize=18)
+        for i in range(x.shape[0]):
+            if y[i] == 0:
+                plt.plot(x[i][0], x[i][1], 'ro')
+            else:
+                plt.plot(x[i][0], x[i][1], 'bo')
+
+        plt.subplot(1, 2, 2)
+        plt.title('Predict result', fontsize=18)
+        for i in range(x.shape[0]):
+            if pred_y[0][i] == 0:
+                plt.plot(x[i][0], x[i][1], 'ro')
+            else:
+                plt.plot(x[i][0], x[i][1], 'bo')
+
+    @staticmethod
+    def learning_curve(costs): 
+        plt.title('Learning Curve', fontsize=18)
+        plt.plot([i for i in range(len(costs))], costs)
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
 
 if __name__ == '__main__':
-    x_train, y_train= DataGenerator('Linear').get_data(1000)
-    x_test, y_test = DataGenerator('Linear').get_data(100)
-    layer_dims = [2, 10, 10, 1]
+    Data = DataGenerator('XOR')
+    x_train, y_train= Data.get_data(1000)
+    x_test, y_test = Data.get_data(100) 
 
+    Network = NN(layer_dims=[2, 10, 10, 1], epoch=10000, lr=0.1, print_step=1000)
     print("---Start training---\n")
-    parameters = model(x_train, y_train, layer_dims, 10000, 0.1, 1000)
+    parameters, costs = Network.train(x_train, y_train)
+    plt.figure(1)
+    NN.learning_curve(costs)
     print("---Training finished---\n")
 
     print("Train result:\n")
-    train_AL, train_pred = predict(x_train, y_train, parameters)
-    
+    train_AL, train_pred = Network.predict(x_train, y_train, parameters)
     print("---Start testing---\n")    
     print("Test result:\n")
-    test_AL, test_pred = predict(x_test, y_test, parameters)
+    test_AL, test_pred = Network.predict(x_test, y_test, parameters)
     print("---Testing finished---\n")
     
     print("Test Prediction:\n")
     print(test_AL.reshape(-1, 1))
-    show_result(x_train, y_train, train_pred)
-    show_result(x_test, y_test, test_pred)
+
+    plt.figure(2)
+    NN.show_result(x_train, y_train, train_pred)
+
+    plt.figure(3)
+    NN.show_result(x_test, y_test, test_pred)
 # %%
