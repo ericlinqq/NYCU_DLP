dea0a00d9b287e041eb930265906bc0e09560f5c
ericlin66 <fictionman66@icloud.com>
2022-07-22 02:03:34 +0800

    Looks good! wait for training

--- lab3/ResNet.py
+++ lab3/ResNet.py
@@ -1,18 +1,32 @@
 import torch.nn as nn
 from torchvision.models import resnet18, resnet50
+import torch
+import time
+import copy
 
 class ResNet(nn.Module):
-    def __init__(self, num_classes=5, type=0, weights=None, feature_extract=True):
+    def __init__(self, num_classes=5, net_type=18, weights=None, feature_extract=False):
         super(ResNet, self).__init__()
+
+        input_size = 224
+
+        self.feature_extract = feature_extract
         
-        if type:
+        if weights is not None:
+            self.mode = 'pretrained'
+        else:
+            self.mode = 'random_init'
+
+        self.net_type = net_type
+
+        if net_type == 50:
             self.model = resnet50(weights=weights)
         else:
             self.model = resnet18(weights=weights)
         
-        set_paramete_require_grad(self.model, feature_extract)
-        num_ftrs = self.model.fc.in_features
-        self.model.fc = nn.Linear(num_ftrs, num_classes)
+        set_paramete_require_grad(self.model, self.feature_extract)
+        self.num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Linear(self.num_ftrs, num_classes)
     
     def forward(self, x):
         x = self.model(x)
@@ -23,3 +37,68 @@ def set_paramete_require_grad(model, feature_extracting):
     if feature_extracting:
         for param in model.parameters():
             param.require_grad = False
+
+
+def train(model, dataloaders, criterion, optimizer, num_epochs, device):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    train_acc_history = []
+    test_acc_history = []
+
+    for epoch in range(1, num_epochs+1):
+        print(f"Epoch {epoch}/{num_epochs}")
+        print("-" * 10)
+
+        for phase in ['train', 'test']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+            
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+
+                    _, pred = torch.max(outputs, 1)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(pred == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
+
+            if phase == 'test':
+                test_acc_history.append(epoch_acc)
+                
+                if epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    best_model_wts = copy.deepcopy(model.state_dict())
+            else:
+                train_acc_history.append(epoch_acc)
+            
+        print()
+
+    time_elapsed = time.time() - since
+    print(f"Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}")
+    print(f"Best Test Acc: {best_acc:.4f}")
+
+    model.load_state_dict(best_model_wts) 
+
+    return model, train_acc_history, test_acc_history
--- lab3/main.py
+++ lab3/main.py
@@ -1,72 +1,99 @@
+from ResNet import ResNet, train
+from torchvision import transforms
 from dataloader import RetinopathyLoader
-from ResNet import ResNet
-import torch.nn as nn
+from torch.utils.data import DataLoader
 import torch
-import argparse
-import time
-import copy
+import torch.optim as optim
+import torch.nn  as nn
+from torchvision.models import ResNet50_Weights, ResNet18_Weights
+import pandas as pd
+import matplotlib.pyplot as plt
+from argparser import parse_argument
 
-def train(model, dataloaders, criterion, optimizer, num_epochs):
-    since = time.time()
+def main():
+    data_transform = {
+        'train':transforms.Compose([
+            transforms.RandomResizedCrop(ResNet.input_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'test':transforms.Compose([
+            transforms.Resize(ResNet.input_size),
+            transforms.CenterCrop(ResNet.input_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    }
 
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-    train_acc_history = []
-    test_acc_history = []
+    print(f"Initializing Datasets and Dataloaders...")
 
-    for epoch in range(1, num_epochs+1):
-        print(f"Epoch {epoch}/{num_epochs}")
-        print("-" * 10)
+    image_dataset = {x: RetinopathyLoader('data/', x, data_transform[x]) for x in ['train', 'test']}
 
-        for phase in ['train', 'test']:
-            if phase == 'train':
-                model.train()
-            else:
-                model.eval()
-            
-            running_loss = 0.0
-            running_corrects = 0
+    dataloaders_dict = {x: DataLoader(image_dataset[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['train', 'test']}
 
-            for inputs, labels in dataloaders[phase]:
-                inputs = inputs.to(device)
-                labels = labels.to(device)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-                optimizer.zero_grad()
+    pretrained_model = ResNet(num_classes=num_classes, net_type=net_type, weights=weights, feature_extract=feature_extract)
+    random_init_model = ResNet(num_classes=num_classes, net_type=net_type, weights=None, feature_extract=False)
 
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    loss = criterion(outputs, labels)
+    df = pd.Datafrme()
+    df['Epoch'] = range(1, num_epochs+1)
 
-                    _, pred = torch.max(outputs, 1)
+    for model in [pretrained_model, random_init_model]:
+        print(f"ResNet{model.type}({model.mode})")
+        model = model.to(device)
 
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
+        params_to_update = model.parameters()
+        print("Params to learn:")
 
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(pred == labels.data)
+        if model.feature_extract:
+            params_to_update = []
+            for name, param in model.named_parameters():
+                if param.requires_grad == True:
+                    params_to_update.append(param)
+                    print(f"\t{name}")
+        else:
+            for name, param in model.named_parameters():
+                if param.requires_grad == True:
+                    print(f"\t{name}")
 
-            epoch_loss = running_loss / len(dataloaders[phase].dataset)
-            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+        optimizer = optim.SGD(params_to_update, lr=lr, momentum=momentum, weight_decay=weight_decay)
 
-            print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
+        criterion = nn.CrossEntropyLoss()
 
-            if phase == 'test':
-                test_acc_history.append(epoch_acc)
-                
-                if epoch_acc > best_acc:
-                    best_acc = epoch_acc
-                    best_model_wts = copy.deepcopy(model.state_dict())
-            else:
-                train_acc_history.append(epoch_acc)
-            
-        print()
+        model, train_acc_history, test_acc_history = train(model, dataloaders_dict, criterion, optimizer, num_epochs=num_epochs, device=device)
 
-    time_elapsed = time.time() - since
-    print(f"Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}")
-    print(f"Best Test Acc: {best_acc:.4f}")
+        model_scripted = torch.jit.script(model)
+        model_scripted.save(f'model/ResNet{model.net_type}/{model.mode}.pt')
 
-    model.load_state_dict(best_model_wts)
+        df[f'{model.mode}_train_acc'] = train_acc_history
+        df[f'{model.mode}_test_acc'] = test_acc_history
 
-    return model, train_acc_history, test_acc_history
+    df.set_index('Epoch', inplace=True)
+    df.to_csv(f'output/ResNet{model.net_type}/accuracy.csv')
+    df.plot(title=f'Result Comparison (ResNet{model.net_type})', 
+            xlabel='Epochs',
+            ylabel='Accuracy(%)',
+            legend=True,
+            figsize=(10, 5)
+    )
+    plt.savefig(f"output/ResNet{model.net_type}/comparison.png")
 
+if __name__ == '__main__':
+    args = parse_argument()
+    batch_size = args.b
+    num_classes = args.c
+    net_type = args.t
+    feature_extract = args.f
+    num_epochs = args.e
+    lr = args.l
+    momentum = args.m
+    weight_decay = args.w
+
+    if net_type == 18:
+        weights = ResNet18_Weights.DEFAULT
+    else:
+        weights = ResNet50_Weights.DEFAULT
+
+    main()
