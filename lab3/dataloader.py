1bb5c9d13f092b03b78e4805753caeeeeb698853
ericlin66 <fictionman66@icloud.com>
2022-07-21 19:25:49 +0800

    gitignore data folder

--- lab3/dataloader.py
+++ lab3/dataloader.py
@@ -37,20 +37,20 @@ class RetinopathyLoader(data.Dataset):
         """something you should implement here"""
 
         """
-           step1. Get the image path from 'self.img_name' and load it.
-                  hint : path = root + self.img_name[index] + '.jpeg'
-           
-           step2. Get the ground truth label from self.label
-                     
-           step3. Transform the .jpeg rgb images during the training phase, such as resizing, random flipping, 
-                  rotation, cropping, normalization etc. But at the beginning, I suggest you follow the hints. 
-                       
-                  In the testing phase, if you have a normalization process during the training phase, you only need 
-                  to normalize the data. 
-                  
-                  hints : Convert the pixel value to [0, 1]
-                          Transpose the image shape from [H, W, C] to [C, H, W]
-                         
+            step1. Get the image path from 'self.img_name' and load it.
+                hint : path = root + self.img_name[index] + '.jpeg'
+        
+            step2. Get the ground truth label from self.label
+            
+            step3. Transform the .jpeg rgb images during the training phase, such as resizing, random flipping, 
+                                                    rotation, cropping, normalization etc. But at the beginning, I suggest you follow the hints. 
+                    
+                    In the testing phase, if you have a normalization process during the training phase, you only need 
+                    to normalize the data. 
+                
+                    hints : Convert the pixel value to [0, 1]
+                            Transpose the image shape from [H, W, C] to [C, H, W]
+                        
             step4. Return processed image and label
         """
 
