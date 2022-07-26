664e79369ccc00e824c45aff6e767b7ea1d345d5
ericlinqq <fictionman66@icloud.com>
2022-07-26 12:57:56 +0800

    Add lab4

--- lab3/dataloader.py
+++ lab3/dataloader.py
@@ -25,6 +25,7 @@ class RetinopathyLoader(data.Dataset):
 
             self.img_name (string list): String list that store all image names.
             self.label (int or float list): Numerical list that store all ground truth label values.
+            self.transforms (torchvision.transforms): Data transforms function.
         """
         self.root = root
         self.img_name, self.label = getData(mode)
@@ -37,24 +38,6 @@ class RetinopathyLoader(data.Dataset):
         return len(self.img_name)
 
     def __getitem__(self, index):
-        """something you should implement here"""
-        """
-            step1. Get the image path from 'self.img_name' and load it.
-                hint : path = root + self.img_name[index] + '.jpeg'
-        
-            step2. Get the ground truth label from self.label
-            
-            step3. Transform the .jpeg rgb images during the training phase, such as resizing, random flipping, 
-                    rotation, cropping, normalization etc. But at the beginning, I suggest you follow the hints. 
-                    
-                    In the testing phase, if you have a normalization process during the training phase, you only need 
-                    to normalize the data. 
-                
-                    hints : Convert the pixel value to [0, 1]
-                            Transpose the image shape from [H, W, C] to [C, H, W]
-                        
-            step4. Return processed image and label
-        """
         img_path = os.path.join(self.root, f'{self.img_name[index]}.jpeg')
         img = Image.open(img_path)
 
