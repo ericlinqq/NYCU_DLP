bcf2c663d06b2e6652c61b1c96f5f2fa1adae33d
ericlin66 <fictionman66@icloud.com>
2022-07-27 18:07:00 +0800

    class-weighted loss expriment done

--- lab3/main.py
+++ lab3/main.py
@@ -146,19 +146,19 @@ def main():
         
         name = model.mode.replace("/", "")
         
-        model_scripted.save(f"model/ResNet{model.net_type}/model({name})_cw.pt")
+        model_scripted.save(f"model/ResNet{model.net_type}/model({name}).pt")
 
         plt.figure()
         disp = ConfusionMatrixDisplay(confusion_matrix=cm)
         disp.plot()
-        plt.savefig(f"output/ResNet{model.net_type}/confusion_matrix({name})_cw.png")
+        plt.savefig(f"output/ResNet{model.net_type}/confusion_matrix({name}).png")
         plt.close()
 
         df[f'Train({model.mode})'] = train_acc_history
         df[f'Test({model.mode})'] = test_acc_history
 
     df.set_index('Epoch', inplace=True)
-    df.to_csv(f'output/ResNet{model.net_type}/accuracy_cw.csv')
+    df.to_csv(f'output/ResNet{model.net_type}/accuracy.csv')
     plt.figure()
     df.plot(title=f'Result Comparison (ResNet{model.net_type})', 
             xlabel='Epochs',
@@ -166,7 +166,7 @@ def main():
             legend=True,
             figsize=(10, 5)
     )
-    plt.savefig(f"output/ResNet{model.net_type}/comparison_cw.png")
+    plt.savefig(f"output/ResNet{model.net_type}/comparison.png")
     plt.close()
 
 def parse_argument():
