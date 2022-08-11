cd08f7145f31f924a820249f386c49107a58c3dc
eric <ericlin66.cs11@nycu.edu.tw>
2022-08-11 17:04:36 +0800

    learned_prior.py

---
+++ lab4/train_learned_prior.py
@@ -0,0 +1,419 @@
+import argparse
+import itertools
+import os
+import random
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from dataset import bair_robot_pushing_dataset
+from models.lstm import gaussian_lstm, lstm
+from models.vgg_64 import vgg_decoder, vgg_encoder
+from utils import init_weights, finn_eval_seq, plot_curve, save_gif
+from torchvision.utils import save_image, make_grid
+
+torch.backends.cudnn.benchmark = True
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--lr', default=0.002, type=float, help='learning rate')
+    parser.add_argument('--beta1', default=0.9, type=float, help='momentum term for adam')
+    parser.add_argument('--batch_size', default=12, type=int, help='batch size')
+    parser.add_argument('--log_dir', default='./logs/lp', help='base directory to save logs')
+    parser.add_argument('--model_dir', default='', help='base directory to save logs')
+    parser.add_argument('--data_root', default='./data/processed_data', help='root directory for data')
+    parser.add_argument('--optimizer', default='adam', help='optimizer to train with')
+    parser.add_argument('--niter', type=int, default=300, help='number of epochs to train for')
+    parser.add_argument('--epoch_size', type=int, default=600, help='epoch size')
+    parser.add_argument('--tfr', type=float, default=1.0, help='teacher forcing ratio (0 ~ 1)')
+    parser.add_argument('--tfr_start_decay_epoch', type=int, default=0, help='The epoch that teacher forcing ratio become decreasing')
+    parser.add_argument('--tfr_decay_step', type=float, default=0, help='The decay step size of teacher forcing ratio (0 ~ 1)')
+    parser.add_argument('--tfr_lower_bound', type=float, default=0, help='The lower bound of teacher forcing ratio for scheduling teacher forcing ratio (0 ~ 1)')
+    parser.add_argument('--kl_anneal_cyclical', default=False, action='store_true', help='use cyclical mode')
+    parser.add_argument('--kl_anneal_ratio', type=float, default=0.5, help='The decay ratio of kl annealing')
+    parser.add_argument('--kl_anneal_cycle', type=int, default=1, help='The number of cycle for kl annealing during training (if use cyclical mode)')
+    parser.add_argument('--seed', default=1, type=int, help='manual seed')
+    parser.add_argument('--n_past', type=int, default=2, help='number of frames to condition on')
+    parser.add_argument('--n_future', type=int, default=10, help='number of frames to predict')
+    parser.add_argument('--n_eval', type=int, default=30, help='number of frames to predict at eval time')
+    parser.add_argument('--rnn_size', type=int, default=256, help='dimensionality of hidden layer')
+    parser.add_argument('--prior_rnn_layers', type=int, default=1, help='number of layers')
+    parser.add_argument('--posterior_rnn_layers', type=int, default=1, help='number of layers')
+    parser.add_argument('--predictor_rnn_layers', type=int, default=2, help='number of layers')
+    parser.add_argument('--z_dim', type=int, default=64, help='dimensionality of z_t')
+    parser.add_argument('--g_dim', type=int, default=128, help='dimensionality of encoder output vector and decoder input vector')
+    parser.add_argument('--beta', type=float, default=0.0001, help='weighting on KL to prior')
+    parser.add_argument('--num_workers', type=int, default=4, help='number of data loading threads')
+    parser.add_argument('--last_frame_skip', action='store_true', help='if true, skip connections go between frame t and frame t+t rather than last ground truth frame')
+    parser.add_argument('--cuda', default=False, action='store_true')  
+    parser.add_argument('--cond_dim', type=int, default=7, help='dimensionality of condition')
+
+    args = parser.parse_args()
+    return args
+
+def kl_criterion(mu1, logvar1, mu2, logvar2, args):
+    # KL( N(mu_1, sigma2_1) || N(mu_2, sigma2_2)) = 
+    #   log( sqrt(
+    # 
+    sigma1 = logvar1.mul(0.5).exp() 
+    sigma2 = logvar2.mul(0.5).exp() 
+    kld = torch.log(sigma2/sigma1) + (torch.exp(logvar1) + (mu1 - mu2)**2)/(2*torch.exp(logvar2)) - 1/2
+    return kld.sum() / args.batch_size
+
+def train(x, cond, modules, optimizer, kl_anneal, args):
+    modules['frame_predictor'].zero_grad()
+    modules['posterior'].zero_grad()
+    modules['prior'].zero_grad()
+    modules['encoder'].zero_grad()
+    modules['decoder'].zero_grad()
+
+    # initialize the hidden state.
+    modules['frame_predictor'].hidden = modules['frame_predictor'].init_hidden()
+    modules['posterior'].hidden = modules['posterior'].init_hidden()
+    modules['prior'].hidden = modules['prior'].init_hidden()
+
+    mse = 0
+    kld = 0
+    use_teacher_forcing = True if random.random() < args.tfr else False
+    mse_criterion = nn.MSELoss()
+    
+    x_pred = x[0]
+    for i in range(1, args.n_past + args.n_future): 
+        h_target, _ = modules['encoder'](x[i])
+
+        if use_teacher_forcing:
+            h = modules['encoder'](x[i-1])
+        else:
+            h = modules['encoder'](x_pred)
+
+        if args.last_frame_skip or i < args.n_past:
+            h, skip = h
+        else:
+            h, _ = h
+        
+        z_t, mu, logvar = modules['posterior'](h_target)
+        _, mu_p, logvar_p = modules['prior'](h)
+        h_pred = modules['frame_predictor'](torch.cat([cond[i], h, z_t], 1))
+        x_pred = modules['decoder']([h_pred, skip])
+
+        mse += mse_criterion(x_pred, x[i])
+        kld += kl_criterion(mu, logvar, mu_p, logvar_p, args)
+            
+
+    beta = kl_anneal.get_beta()
+    loss = mse + kld * beta
+    loss.backward()
+
+    optimizer.step()
+
+    return beta, loss.detach().cpu().numpy() / (args.n_past + args.n_future), mse.detach().cpu().numpy() / (args.n_past + args.n_future), kld.detach().cpu().numpy() / (args.n_future + args.n_past)
+
+def pred(x, cond, modules, args, device):
+    modules['frame_predictor'].hidden = modules['frame_predictor'].init_hidden()
+    # modules['posterior'].hidden = modules['posterior'].init_hidden()
+    modules['prior'].hidden = modules['prior'].init_hidden()
+    pred_seq = [x[0]]
+    x_pred = x[0]
+    for i in range(1, args.n_eval):
+        h = modules['encoder'](x_pred)
+        if args.last_frame_skip or i < args.n_past:
+            h, skip = h
+        else:
+            h, _ = h
+
+        if i < args.n_past:
+            h_target, _ = modules['encoder'](x[i])
+            z_t, _, _ = modules['posterior'](h_target)
+            modules['prior'](h)
+            modules['frame_predictor'](torch.cat([cond[i], h, z_t], 1))
+            x_pred = x[i]
+        else:
+            z_t, _, _ = modules['prior'](h)
+            h = modules['frame_predictor'](torch.cat([cond[i], h, z_t], 1))
+            x_pred = modules['decoder']([h, skip])
+        pred_seq.append(x_pred) 
+
+    return pred_seq
+
+def plot_pred(x, cond, modules, epoch, args, device):
+    gt_seq = [x[i] for i in range(len(x))]
+    pred_seq = pred(x, cond, modules, args, device)
+
+    pred_plot = []
+    gt_plot = []
+    gif = [[] for t in range(args.n_eval)]
+
+    i = random.randint(0, args.batch_size-1)
+    for t in range(args.n_eval):
+        pred_plot.append(pred_seq[t][i])
+        gt_plot.append(gt_seq[t][i])
+        gif[t].append(gt_seq[t][i])
+        gif[t].append(pred_seq[t][i])
+
+    fname = f'{args.log_dir}/gen/sample_{epoch}'
+    save_image(make_grid(pred_plot, nrow=args.n_eval), fname+'_pred.png')
+    save_image(make_grid(gt_plot, nrow=args.n_eval), fname+'_gt.png')
+    save_gif(fname+'.gif', gif, duration=0.25)
+
+    return pred_seq
+
+class kl_annealing():
+    def __init__(self, args):
+        if not args.kl_anneal_cyclical:
+            args.kl_anneal_cycle = 1 
+        period = args.niter / args.kl_anneal_cycle
+        step = 1. / (period * args.kl_anneal_ratio)
+        self.L = np.ones(args.niter)
+        self.idx = 0
+        
+        for c in range(args.kl_anneal_cycle):
+            v, i = 0.0, 0
+            while v <= 1.0 and (int(i+c*period) < args.niter):
+                self.L[int(i+c*period)] = v
+                v += step
+                i += 1
+
+    def update(self):
+        if self.idx < len(self.L)-1:
+            self.idx += 1
+
+    def get_beta(self):
+        beta = self.L[self.idx]
+        return beta
+
+def main():
+    args = parse_args()
+    if args.cuda:
+        assert torch.cuda.is_available(), 'CUDA is not available.'
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+    print(f"Using {device} device")
+
+    assert args.n_past + args.n_future <= 30 and args.n_eval <= 30
+    assert 0 <= args.tfr and args.tfr <= 1
+    assert 0 <= args.tfr_start_decay_epoch 
+    assert 0 <= args.tfr_decay_step and args.tfr_decay_step <= 1
+
+    if args.model_dir != '':
+        # load model and continue training from checkpoint
+        saved_model = torch.load('%s/model.pth' % args.model_dir)
+        optimizer = args.optimizer
+        model_dir = args.model_dir
+        niter = args.niter
+        args = saved_model['args']
+        args.optimizer = optimizer
+        args.model_dir = model_dir
+        args.log_dir = '%s/continued' % args.log_dir
+        start_epoch = saved_model['last_epoch']
+        kl_anneal = saved_model['kl_anneal']
+        plot_record = saved_model['plot_record']
+    else:
+        name = 'rnn_size=%d-predictor-posterior-rnn_layers=%d-%d-n_past=%d-n_future=%d-lr=%.4f-g_dim=%d-z_dim=%d-last_frame_skip=%s-beta=%.7f'\
+            % (args.rnn_size, args.predictor_rnn_layers, args.posterior_rnn_layers, args.n_past, args.n_future, args.lr, args.g_dim, args.z_dim, args.last_frame_skip, args.beta)
+
+        args.log_dir = '%s/%s' % (args.log_dir, name)
+        niter = args.niter
+        start_epoch = 0
+        kl_anneal = kl_annealing(args)
+        plot_record = {"loss": [], "mse": [], "kld": [], "tfr": [], "KL_weight": [], "psnr": []}
+
+    os.makedirs(args.log_dir, exist_ok=True)
+    os.makedirs('%s/gen/' % args.log_dir, exist_ok=True)
+
+    print("Random Seed: ", args.seed)
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+
+    if os.path.exists('./{}/train_record.txt'.format(args.log_dir)):
+        os.remove('./{}/train_record.txt'.format(args.log_dir))
+    
+    print(args)
+
+    with open('./{}/train_record.txt'.format(args.log_dir), 'a') as train_record:
+        train_record.write('args: {}\n'.format(args))
+
+    # ------------ build the models  --------------
+
+    if args.model_dir != '':
+        frame_predictor = saved_model['frame_predictor']
+        posterior = saved_model['posterior']
+        prior = saved_model['prior']
+    else:
+        frame_predictor = lstm(args.cond_dim+args.g_dim+args.z_dim, args.g_dim, args.rnn_size, args.predictor_rnn_layers, args.batch_size, device)
+        posterior = gaussian_lstm(args.g_dim, args.z_dim, args.rnn_size, args.posterior_rnn_layers, args.batch_size, device)
+        prior = gaussian_lstm(args.g_dim, args.z_dim, args.rnn_size, args.prior_rnn_layers, args.batch_size, device)
+        frame_predictor.apply(init_weights)
+        posterior.apply(init_weights)
+        prior.apply(init_weights)
+            
+    if args.model_dir != '':
+        decoder = saved_model['decoder']
+        encoder = saved_model['encoder']
+    else:
+        encoder = vgg_encoder(args.g_dim)
+        decoder = vgg_decoder(args.g_dim)
+        encoder.apply(init_weights)
+        decoder.apply(init_weights)
+    
+    # --------- transfer to device ------------------------------------
+    frame_predictor.to(device)
+    posterior.to(device)
+    prior.to(device)
+    encoder.to(device)
+    decoder.to(device)
+
+    # --------- load a dataset ------------------------------------
+    train_data = bair_robot_pushing_dataset(args, 'train')
+    validate_data = bair_robot_pushing_dataset(args, 'validate')
+    train_loader = DataLoader(train_data,
+                            num_workers=args.num_workers,
+                            batch_size=args.batch_size,
+                            shuffle=True,
+                            drop_last=True,
+                            pin_memory=True)
+    train_iterator = iter(train_loader)
+
+    validate_loader = DataLoader(validate_data,
+                            num_workers=args.num_workers,
+                            batch_size=args.batch_size,
+                            shuffle=True,
+                            drop_last=True,
+                            pin_memory=True)
+
+    validate_iterator = iter(validate_loader)
+
+    # ---------------- optimizers ----------------
+    if args.optimizer == 'adam':
+        args.optimizer = optim.Adam
+    elif args.optimizer == 'rmsprop':
+        args.optimizer = optim.RMSprop
+    elif args.optimizer == 'sgd':
+        args.optimizer = optim.SGD
+    else:
+        raise ValueError('Unknown optimizer: %s' % args.optimizer)
+
+    params = list(frame_predictor.parameters()) + list(posterior.parameters()) + list(encoder.parameters()) + list(decoder.parameters())
+    optimizer = args.optimizer(params, lr=args.lr, betas=(args.beta1, 0.999))
+
+
+    modules = {
+        'frame_predictor': frame_predictor,
+        'posterior': posterior,
+        'prior': prior,
+        'encoder': encoder,
+        'decoder': decoder,
+    }
+    # --------- training loop ------------------------------------
+
+    progress = tqdm(total=args.niter)
+    best_val_psnr = 0
+    args.tfr_decay_step = 1. / (args.niter - args.tfr_start_decay_epoch - 1)
+    for epoch in range(start_epoch, start_epoch + niter):
+        frame_predictor.train()
+        posterior.train()
+        prior.train()
+        encoder.train()
+        decoder.train()
+
+        epoch_loss = 0
+        epoch_mse = 0
+        epoch_kld = 0
+
+        for _ in range(args.epoch_size):
+            try:
+                seq, cond = next(train_iterator)
+            except StopIteration:
+                train_iterator = iter(train_loader)
+                seq, cond = next(train_iterator) 
+
+            seq, cond = seq.transpose_(0, 1).to(device), cond.transpose_(0, 1).to(device)
+
+            beta, loss, mse, kld = train(seq, cond, modules, optimizer, kl_anneal, args)
+            epoch_loss += loss
+            epoch_mse += mse
+            epoch_kld += kld
+
+        plot_record['loss'].append(epoch_loss / args.epoch_size)
+        plot_record['mse'].append(epoch_mse / args.epoch_size)
+        plot_record['kld'].append(epoch_kld / args.epoch_size)
+        plot_record['KL_weight'].append(beta)
+        kl_anneal.update()
+        plot_record['tfr'].append(args.tfr)
+
+        if epoch >= args.tfr_start_decay_epoch:
+            ### Update teacher forcing ratio ###
+            args.tfr -= args.tfr_decay_step
+            if args.tfr < args.tfr_lower_bound:
+                args.tfr = args.tfr_lower_bound
+            
+
+        progress.update(1)
+        with open('./{}/train_record.txt'.format(args.log_dir), 'a') as train_record:
+            train_record.write(('[epoch: %02d] loss: %.5f | mse loss: %.5f | kld loss: %.5f\n' % (epoch, epoch_loss  / args.epoch_size, epoch_mse / args.epoch_size, epoch_kld / args.epoch_size)))
+        
+        frame_predictor.eval()
+        encoder.eval()
+        decoder.eval()
+        posterior.eval()
+        prior.eval()
+
+        if epoch % 5 == 0:
+            psnr_list = []
+            for _ in range(len(validate_data) // args.batch_size):
+                try:
+                    validate_seq, validate_cond = next(validate_iterator)
+                except StopIteration:
+                    validate_iterator = iter(validate_loader)
+                    validate_seq, validate_cond = next(validate_iterator)
+
+                validate_seq, validate_cond = validate_seq.transpose_(0, 1).to(device), validate_cond.transpose_(0, 1).to(device)
+                with torch.no_grad():
+                    pred_seq = pred(validate_seq, validate_cond, modules, args, device)
+                _, _, psnr = finn_eval_seq(validate_seq[args.n_past:], pred_seq[args.n_past:])
+                psnr_list.append(psnr)
+                
+            ave_psnr = np.mean(np.concatenate(psnr_list))
+            plot_record['psnr'].append(ave_psnr)
+
+
+            with open('./{}/train_record.txt'.format(args.log_dir), 'a') as train_record:
+                train_record.write(('====================== validate psnr = {:.5f} ========================\n'.format(ave_psnr)))
+
+            if ave_psnr > best_val_psnr:
+                best_val_psnr = ave_psnr
+                # save the model
+                torch.save({
+                    'encoder': encoder,
+                    'decoder': decoder,
+                    'frame_predictor': frame_predictor,
+                    'posterior': posterior,
+                    'prior': prior,
+                    'args': args,
+                    'last_epoch': epoch,
+                    'kl_anneal': kl_anneal,
+                    'plot_record': plot_record},
+                    '%s/model.pth' % args.log_dir)
+
+        if epoch % 20 == 0:
+            try:
+                validate_seq, validate_cond = next(validate_iterator)
+            except StopIteration:
+                validate_iterator = iter(validate_loader)
+                validate_seq, validate_cond = next(validate_iterator)
+
+            validate_seq, validate_cond = validate_seq.transpose_(0, 1).to(device), validate_cond.transpose_(0, 1).to(device)
+            with torch.no_grad():
+                _ = plot_pred(validate_seq, validate_cond, modules, epoch, args, device)
+    torch.save(plot_record, f"{args.log_dir}/records.pth")
+    plot_curve(plot_record, args.niter, f"{args.log_dir}/curve.png")
+if __name__ == '__main__':
+    print(f"Pytorch version: {torch.__version__}")
+    main()
+        
